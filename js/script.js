45f4421e5a3b365a45dff04ddbc61df544061db8
Алексей <@AlexeyAZ>
2019-05-03 12:33:35 +0700

    Добавлены коментарии, подпилен код

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 var res;
 $(document).ready(function () {
+    //берем значения формы складываем, из-за плавающей точки приводим типы, округляем
     $("#btnSum").click(function () {
         var one = $("#one").val();
         var two = $("#two").val();
         res = parseFloat(one) + parseFloat(two);
         if (isNaN(res)) {
-            alert("Заполните поля");
+            alert('Заполните поля');
         } else {
-            alert("Результат: " + res.toFixed(1));
+            alert('Результат: ' + res.toFixed(1));
             $('#result').text(res.toFixed(1));
         }
     });
