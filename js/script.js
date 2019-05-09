23cef11888528ac6eb08091eb3c530c50762b781
Алексей <@AlexeyAZ>
2019-05-09 17:38:40 +0700

    Изменена маска ввода для сложения четырехзначных чисел с 4 знаками после запятой. Сделан красивый вывод целого числа.

--- js/script.js
+++ js/script.js
@@ -2,16 +2,24 @@ var res;
 $(document).ready(function () {
     //берем значения формы складываем, из-за плавающей точки приводим типы, округляем
     $("#btnSum").click(function () {
-        var one = $("#one").val();
-        var two = $("#two").val();
+        let one = $("#one").val();
+        let two = $("#two").val();
         res = parseFloat(one) + parseFloat(two);
         if (isNaN(res)) {
             alert('Заполните поля');
         } else {
-            alert('Результат: ' + res.toFixed(1));
-            $('#result').text(res.toFixed(1));
+            let vid = res.toFixed(1);
+            //проверяем есть ли остаток чтобы красиво вывести целое число
+            if (vid%1 === 0) {
+                alert('Результат: ' + parseInt(vid));
+                $('#result').text(parseInt(vid));
+            } else {
+                alert('Результат: ' + vid);
+                $('#result').text(vid);
+            }
+
         }
     });
-    $("#one").mask('0.0');
-    $("#two").mask('0.0');
+    $("#one").mask('9999.9999');
+    $("#two").mask('9999.9999');
 });
